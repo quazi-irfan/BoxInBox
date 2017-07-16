e24a3706b5f3ad00b7fb39efdf40e4d3dda2130f
quazi-irfan <quazirfan@gmail.com>
2017-07-16 01:02:22 -0500

    Add jME simple application, world.xbuf

--- src/main/java/net/qirfan/App.java
+++ src/main/java/net/qirfan/App.java
@@ -1,13 +1,38 @@
 package net.qirfan;
 
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Node;
+import jme3_ext_xbuf.XbufLoader;
+
 /**
  * BoxInBox
- *
+ * @author Quazi Irfan
  */
-public class App
+public class App extends SimpleApplication
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App app = new App();
+        app.showSettings = false;
+        app.start();
+    }
+
+    public void simpleInitApp() {
+        flyCam.setMoveSpeed(35f);
+
+        assetManager.registerLoader(XbufLoader.class, "xbuf");
+
+        Node world_Node = (Node) assetManager.loadModel("Models/world.xbuf");
+        Material world_Material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        world_Material.setColor("Color", ColorRGBA.Blue);
+        world_Node.setMaterial(world_Material);
+        rootNode.attachChild(world_Node);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        super.simpleUpdate(tpf);
     }
 }
