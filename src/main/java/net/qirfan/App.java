96067c83379697543695f82c35b89f8d3003abe2
quazi-irfan <quazirfan@gmail.com>
2017-08-13 21:16:59 -0500

    Remove xbuf dependency. Setting will show up at start.

--- src/main/java/net/qirfan/App.java
+++ src/main/java/net/qirfan/App.java
@@ -5,8 +5,8 @@ import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
-import com.jme3.scene.Node;
-import jme3_ext_xbuf.XbufLoader;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
 
 /**
  * BoxInBox
@@ -17,14 +17,12 @@ public class App extends SimpleApplication
     public static void main( String[] args )
     {
         App app = new App();
-        app.showSettings = false;
+//        app.showSettings = false;
         app.start();
     }
 
     public void simpleInitApp() {
         flyCam.setMoveSpeed(35f);
-
-        assetManager.registerLoader(XbufLoader.class, "xbuf");
 
         makeCubes(25);
     }
@@ -35,18 +33,18 @@ public class App extends SimpleApplication
     }
 
     private void makeCubes(int number){
-        Node boxNode;
+        Geometry boxGeo;
         Vector3f boxLoc;
         Material boxMat;
 
         for(int i = 0; i<number; i++){
-            boxNode = (Node) assetManager.loadModel("Models/world.xbuf");
+            boxGeo = new Geometry("BoxGeometry", new Box(1,1,1));
             boxMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
             boxMat.setColor("Color", ColorRGBA.randomColor());
-            boxNode.setMaterial(boxMat);
-            rootNode.attachChild(boxNode);
+            boxGeo.setMaterial(boxMat);
+            rootNode.attachChild(boxGeo);
             boxLoc = new Vector3f(FastMath.nextRandomInt(-10, 10),FastMath.nextRandomInt(-10, 10),FastMath.nextRandomInt(-10, 10));
-            boxNode.setLocalTranslation(boxLoc);
+            boxGeo.setLocalTranslation(boxLoc);
         }
     }
 }
